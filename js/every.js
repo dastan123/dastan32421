15b4bb1449dceb386ed06fadcf32e46808229e46
= <dastan.emilbekov0101@mail.ru>
2021-03-10 18:47:17 +0600

    server

---
+++ js/every.js
@@ -0,0 +1,38 @@
+'use strict';
+const forms = document.querySelector('.form'),
+      btn = document.querySelector('.logo-tag');
+
+const message = {
+      loading: 'Loading',
+      success: 'Succesfully loaded',
+      fail: "Failde"
+};
+
+postData(forms);
+
+function postData(form) {
+      form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const statusMessage = document.createElement('div');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'js/index.php');
+            // request.setRequestHeader('Content-type',);
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                  if (request.status === 200) {
+                        console.log(request.response);
+                        statusMessage.textContent = message.success;
+                  } else {
+                        statusMessage.textContent = message.fail;
+                  }
+            });
+      });
+
+
+
+}
