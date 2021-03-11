44342b40dc2d9202051a637403b69ee4b3a94d50
= <dastan.emilbekov0101@mail.ru>
2021-03-11 16:11:32 +0600

    useless commit

--- js/every.js
+++ js/every.js
@@ -1,6 +1,11 @@
 'use strict';
+
+const success = document.querySelector('.success-content'),
+failure = document.querySelector('.fail-content');
+
 const forms = document.querySelector('.form'),
-      btn = document.querySelector('.logo-tag');
+      btn = document.querySelector('.logo-tag'),
+      modalContent = document.querySelector('.modal-content');
 
 const message = {
       loading: 'Loading',
@@ -27,15 +32,53 @@ function postData(form) {
                   if (request.status === 200) {
                         console.log(request.response);
                         statusMessage.textContent = message.success;
-                        form.reset();
+                        // form.reset();
                         setTimeout(() => {
                               statusMessage.remove();
                         }, 2000);
+                        showModal(success);
                   } else {
                         statusMessage.textContent = message.fail;
+                        form.reset(); 
+                        showModal(failure);
+                        hideModal(success);
                   }
             });
       });
+ }
 
 
+
+ // Modal 
+
+ const btnModal = document.querySelector('.btn-modal'),
+       modal = document.querySelector('.modal');
+
+ function showModal(item) {
+       item.classList.add('show');
+       item.classList.remove('hide');
+ }
+
+ function hideModal(item) {
+      item.classList.add('hide');
+      item.classList.remove('show');
 }
+
+hideModal(modal);
+
+btnModal.addEventListener('click', e => {
+      hideModal(success);
+      hideModal(failure);
+      if (modal.classList.contains('hide')) {
+            showModal(modal);
+      } else {
+            hideModal(modal);
+      } 
+      if (forms.classList.contains('hide')) {
+            showModal(forms);
+      } else {
+            hideModal(forms);
+      }
+});
+
+ // Modal result 
